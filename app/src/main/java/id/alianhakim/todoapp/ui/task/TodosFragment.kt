2929e0a6d25328bc028ecf35bef8e5e46bdc63dd
Alian Hakim <alianhakim9@gmail.com>
2022-09-06 08:14:56 +0700

    setup navigation component

--- app/src/main/java/id/alianhakim/todoapp/ui/task/TodosFragment.kt
+++ app/src/main/java/id/alianhakim/todoapp/ui/task/TodosFragment.kt
@@ -1,8 +1,31 @@
 package id.alianhakim.todoapp.ui.task
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import id.alianhakim.todoapp.R
+import id.alianhakim.todoapp.databinding.FragmentTodosBinding
 
 class TodosFragment : Fragment(R.layout.fragment_todos) {
 
+    private var _binding: FragmentTodosBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentTodosBinding.bind(view)
+
+        binding.fabAddTask.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupRecyclerView() {
+
+    }
 }
